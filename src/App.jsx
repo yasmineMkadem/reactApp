bf6758a5bf29fef3d43f3f59a65905f1ee5eb471
mariem mkadem <mmkadem11@gmail.com>
2022-01-23 21:48:19 +0100

    Setting image

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ function App() {
   return (
      <div className="App">
       <h1 className="App-header">Yasmine Mkadem</h1>
+      <div className="">
       <img src={scrImage} alt="img"/>
-      <img src={'..public/imageInPublic.jpeg'} alt="img"/>
+      </div>
+      <div>
+      <img src={process.env.PUBLIC_URL +'/imageInPublic.jpeg'} alt="img"/>
+      </div>
       <div>
       <video className='App-video' controls>
       <source src="myVideo.mp4" type="video/mp4" />

95b304586d8997a313f202ab9639728b7596e81a
mariem mkadem <mmkadem11@gmail.com>
2022-01-23 21:20:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import scrImage from './imageInSrc.jpeg';
+import './style.css';
+
+function App() {
+  return (
+     <div className="App">
+      <h1 className="App-header">Yasmine Mkadem</h1>
+      <img src={scrImage} alt="img"/>
+      <img src={'..public/imageInPublic.jpeg'} alt="img"/>
+      <div>
+      <video className='App-video' controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+      </div>
+    </div>
+  );
+}
+
+export default App;
